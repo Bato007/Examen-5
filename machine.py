cbc85f991d69341b4d7f81558e69c14d9df8c33f
Brandon <brandonhernandez_10@outlook.es>
2021-11-11 17:44:24 -0600

    Finished

--- machine.py
+++ machine.py
@@ -1,14 +1,81 @@
 class Machine(object):
     def __init__(self, parts):
-        self.states = parts['states'] 
-        self.iAlphabet = parts['iAlphabet']
-        self.cAlphabet = parts['cAlphabet']
-        self.transitions = parts['transitions']
-        self.start = parts['start']
-        self.acceptance = parts['acceptance']
-        self.reject = parts['reject']
+        self.__finished__ = False
+        self.__states__ = parts['states'] 
+        self.__iAlphabet__ = parts['iAlphabet']
+        self.__cAlphabet__ = parts['cAlphabet']
+        self.__transitions__ = parts['transitions']
+        self.__start__ = parts['start']
+        self.__acceptance__ = parts['acceptance']
+        self.__reject__ = parts['reject']
+        self.__chain__ = []
+        self.__head__ = [0, self.__start__]
 
     def __str__(self):
-        machine = 'states: {0}\niAlphabet: {1}\ncAlphabet: {2}\ntransitions: {3}\nstart: {4}\nacceptance: {5}\nreject: {6}'.format(str(self.states), str(self.iAlphabet), str(self.cAlphabet), str(self.transitions), str(self.start), str(self.acceptance), str(self.reject))
+        machine = 'states: {0}\niAlphabet: {1}\ncAlphabet: {2}\ntransitions: {3}\nstart: {4}\nacceptance: {5}\nreject: {6}'.format(str(self.__states__), str(self.__iAlphabet__), str(self.__cAlphabet__), str(self.__transitions__), str(self.__start__), str(self.__acceptance__), str(self.__reject__))
         return machine
 
+    def __left__(self, next):
+        if self.__head__[0] == 0: return    # No se puede ir antes
+        self.__head__ = [self.__head__[0] - 1, next['finish']]
+
+    def __right__(self, next):
+        if self.__head__[0] == len(self.__chain__) - 1:
+            self.__chain__.append(' ')
+        self.__head__ = [self.__head__[0] + 1, next['finish']]
+
+    def loadChain(self, chain):
+        self.__chain__ = [letter for letter in chain]
+        self.__head__ = [0, self.__start__]
+        return self.__start__
+
+    # Moves the turing 
+    def move(self):
+        if self.__finished__: return [False, self.__head__[0], self.__head__[1], self.__chain__]
+        next = None
+
+        # Verificando a donde se debe de mover
+        for transition in self.__transitions__:
+            if (transition['start'] == self.__head__[1]) and (transition['alph'] == self.__chain__[self.__head__[0]]):
+                next = transition
+
+        if (next):
+            if (next['finish'] == self.__acceptance__) or (next['finish'] == self.__reject__): 
+                self.__finished__ = True
+
+            if (next['direction'] == 'L'): 
+                self.__left__(next)
+            else: 
+                self.__right__(next)
+
+        return [True, self.__head__[0], next, self.__chain__[:]]
+
+def main():
+    r = Machine({
+    "states": ["q0", "q1", "q2", "q3", "qa", "qr"],
+    "iAlphabet": ["0", "1"],
+    "cAlphabet": ["0", "1", " "],
+    "transitions": [{
+        "start": "q0",
+        "alph":  "0",
+        "finish": "q1",
+        "direction": "R"
+    },
+    {
+        "start": "q1",
+        "alph":  " ",
+        "finish": "qr",
+        "direction": "R"
+    }],
+    "start": "q0",
+    "acceptance": "qa",
+    "reject": "qr"
+    })
+
+    r.loadChain('0')
+    print(r.move())
+    print(r.move())
+    print(r.move())
+
+if __name__ == '__main__':
+    main()
--- turing.py
+++ turing.py
@@ -6,9 +6,30 @@ def loadMachine(file='parts'):
         return json.load(outline)
 
 def main():
+    MAX_ITERATIONS = 20000
+    iteration = 0
+    toTxt = []
     machine = Machine(loadMachine())
-    chain = input('Ingrese la cadena a evaluar: ')
+    # chain = input('Ingrese la cadena a evaluar: ')
+    chain = 'aa'
+    start = machine.loadChain(chain)
+    flag = True
+    toTxt.append((str(start) + chain + '\n'))
 
+    while flag and iteration < MAX_ITERATIONS:
+        flag, index, target, string = machine.move()
+        if flag: 
+            string.insert(index, target['finish'])
+            res = ''.join(string)
+            toTxt.append(res+'\n')
+        iteration += 1
+        if 0 == iteration%100:
+            print(iteration)
+
+    if (iteration == MAX_ITERATIONS): toTxt.append('LOOP')
+
+    with open('result.txt', 'w') as file:
+        file.writelines(toTxt)
 
 if __name__ == '__main__':
     main()
